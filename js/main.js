125a3cc85c31521efd00b396b8d8b670bc54cf7b
Stancia94 <staskobenko88@gmail.com>
2024-12-09 11:43:21 +0300

    2 commit

--- js/main.js
+++ js/main.js
@@ -2,36 +2,56 @@ const inputTaskTextElement = document.querySelector('.input')
 const submitButton = document.getElementById('submitTask')
 const formTask = document.forms.formTask
 const listTask = document.querySelector('.list')
+    
+console.log('hui');
 
+function createTask(event){
 
+  const newTask = document.createElement('div')
+  newTask.classList.add('list__task')
 
-function createTask( inputTaskTextElement){
-  console.log( inputTaskTextElement.value)
-  inputTaskTextElement.preventDefault()
+  const newCheckbox = document.createElement('input')
+  newCheckbox.type = 'checkbox'
+  newCheckbox.classList.add('checkbox-task', 'list__checkbox')
+  
+  const newCheckboxLabel = document.createElement('label')
+  newCheckboxLabel.append(newCheckbox)
+  newCheckboxLabel.classList.add('checkbox-label', 'list__label')
+  newTask.append(newCheckboxLabel)
 
+  const newTaskInner = document.createElement('p')
+  newTaskInner.textContent = inputTaskTextElement.value
+  newTask.append(newTaskInner)
+
+  listTask.append(newTask)
+
+  inputTaskTextElement.value = ''
+  
+  console.log(event.target[0])
+  event.preventDefault()
 }
 formTask.addEventListener('submit', createTask)
 
 // formTask.addEventListener('submit', (event) => {
 
-//   const newTask = document.createElement('div')
-//   newTask.classList.add('list__task')
+  // const newTask = document.createElement('div')
+  // newTask.classList.add('list__task')
 
-//   const newCheckbox = document.createElement('input')
-//   newCheckbox.type = 'checkbox'
-//   newCheckbox.classList.add('checkbox-task', 'list__checkbox')
+  // const newCheckbox = document.createElement('input')
+  // newCheckbox.type = 'checkbox'
+  // newCheckbox.classList.add('checkbox-task', 'list__checkbox')
   
-//   const newCheckboxLabel = document.createElement('label')
-//   newCheckboxLabel.append(newCheckbox)
-//   newCheckboxLabel.classList.add('checkbox-label', 'list__label')
-//   newTask.append(newCheckboxLabel)
+  // const newCheckboxLabel = document.createElement('label')
+  // newCheckboxLabel.append(newCheckbox)
+  // newCheckboxLabel.classList.add('checkbox-label', 'list__label')
+  // newTask.append(newCheckboxLabel)
 
-//   const newTaskInner = document.createElement('p')
-//   newTaskInner.innerHTML = inputTaskTextElement.value
-//   newTask.append(newTaskInner)
+  // const newTaskInner = document.createElement('p')
+  // newTaskInner.innerHTML = inputTaskTextElement.value
+  // newTask.append(newTaskInner)
 
-//   listTask.append(newTask)
+  // listTask.append(newTask)
 
-//   inputTaskTextElement.value = ''
-//   event.preventDefault()
+  // inputTaskTextElement.value = ''
+  // event.preventDefault()
 // })
